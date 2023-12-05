a74b823de6b8f29ba00fd4a0f4250d4f37477d6f
aldaonggar <nur.aldaonggar@student.kuleuven.be>
2023-12-05 09:31:18 +0100

    merging

--- view/renderer/renderer.cpp
+++ view/renderer/renderer.cpp
@@ -1,6 +1,24 @@
 #include "renderer.h"
 
-Renderer::Renderer()
-{
+Renderer::Renderer(){}
 
+QPixmap Renderer::renderGameObject(const QSharedPointer<GameObject>& object) {
+
+//    switch (object->getType()) {
+//        case GameObject::Tile:
+//            return renderTile(object);
+//        case GameObject::Doorway:
+//            return renderDoorway(object);
+//        case GameObject::HealthPack:
+//            return renderHealthPack(object);
+//        case GameObject::Protagonist:
+//            return renderProtagonist(object);
+//        case GameObject::Enemy:
+//            return renderEnemy(object);
+//        default:
+//            // Handle default case or unknown types
+//            QPixmap defaultPixmap;
+//            // ... create a default QPixmap
+//            return defaultPixmap;
+//    }
 }
